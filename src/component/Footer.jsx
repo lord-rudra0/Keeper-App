23fe09b7df79bbb0e51b537e8f673250d61101c7
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-03 19:31:11 +0530

    note.js added

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,3 @@
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-
 import React from "react";
 
 function Footer()
@@ -9,7 +6,7 @@ function Footer()
     const years=current_date.getFullYear();
     return (<div>
         <footer>
-        <p>Created By <a href="#">Me</a></p>;
+        <p>Created By <a href="https://my-portfolio-wine-one-81.vercel.app/">Me</a></p>
             <p>copyright {years}</p>
             </footer>
     </div>);
