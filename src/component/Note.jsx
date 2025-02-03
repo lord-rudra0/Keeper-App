2fad1ddb96912c19bf50fbf49f9c4bd744954ccb
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-03 21:43:25 +0530

    arrow function added

--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+// import notes from "../notes"
 
-function Note()
+
+// function Take_Notes(props){
+//     return <div>
+        
+//         <h1>{props.title}</h1>
+//         <p>{props.content}</p>
+//     </div>
+
+// }
+
+
+
+function Note(props)
 {
     return (
         <div className="note">
-            <h1>This is Heading of Note</h1>
-            <p>This is Body</p>
+            {/* <h1>This is Heading of Note</h1>
+            <p>This is Body</p> */}
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
