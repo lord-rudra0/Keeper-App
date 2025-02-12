064fa4e8865d18afdae64f3ce0aaae9e4e02bfa2
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-13 01:11:59 +0530

    handleChange

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,12 +3,17 @@ import Footer from "./Footer.jsx"
 import Header from "./Header.jsx"
 import Note from "./Note.jsx"
 import notes from "../notes.js";
+import CreateArea from "./CreateArea.jsx";
 
 
 
 
 function App() {
     return (<div><Header />
+        <CreateArea />
+        {/* // takeTitle={takeTitle}
+        // takeContent={takeContent} */}
+
         {notes.map((note) => <Note
             key={note.key}
             id={note.key}
@@ -20,3 +25,17 @@ function App() {
 }
 
 export default App;
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
--- src/component/CreateArea.jsx
+++ src/component/CreateArea.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const [note, setNote] = useState({ takeTitle: "", takeContent: "" });
+
+  function handleChange(e) {
+    setNote(e.target.value);
+
+  }
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" placeholder="Title"
+          onChange={handleChange} value={note.takeTitle} />
+
+
+        <textarea name="content" placeholder="Take a note..." rows="3"
+          value={note.takeContent} onChange={handleChange} />
+        <div>{note.takeContent}</div>
+        <div>{note.takeContent}</div>
         <button>Add</button>
       </form>
     </div>
@@ -13,3 +28,11 @@ function CreateArea() {
 }
 
 export default CreateArea;
+
+
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
