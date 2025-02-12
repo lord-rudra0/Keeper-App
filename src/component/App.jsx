b017510be41992340332adca957c99f4998b05c0
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-13 01:15:25 +0530

    merge

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,16 +3,17 @@ import Footer from "./Footer.jsx"
 import Header from "./Header.jsx"
 import Note from "./Note.jsx"
 import notes from "../notes.js";
+import CreateArea from "./CreateArea.jsx";
 
 
 
 
 function App() {
     return (<div><Header />
-        <CreateArea />
-        {/* // takeTitle={takeTitle}
-        // takeContent={takeContent} */}
-
+        <CreateArea
+            takeTitle={takeTitle}
+            takeContent={takeContent}
+        />
         {notes.map((note) => <Note
             key={note.key}
             id={note.key}
