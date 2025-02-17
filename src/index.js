5782ee0aa1a22777fc0d3cb5f94df9f39098c9a3
Rudra Pratap Singh <130925862+lord-rudra0@users.noreply.github.com>
2025-02-17 23:10:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import App from "./component/App"
-// import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-// import DeleteIcon from '@mui/icons-material/Delete';
 
 
 
